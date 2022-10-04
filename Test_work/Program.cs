864880ebb5e4e3dc96222d5c026903d23622e8cb
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-10-04 10:42:57 +0300

    Final commit

--- Test_work/Program.cs
+++ Test_work/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿Console.Clear();
 Console.WriteLine ("Enter a space-separated string array: "); // Введите массив строк, разделенных пробелом 
 string[] array = Console.ReadLine()!.Split(' '); // Считывание введенного массива и заполнение массива с разделением на подстроки и исключением null значений 
 string[] result = new string[array.Length]; // Массив для вывода результата 
