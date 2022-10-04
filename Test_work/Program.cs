864b06f75f14aa2d9bbd5a39aac7fd9006a0705d
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-10-04 10:18:33 +0300

    Filling the array

--- Test_work/Program.cs
+++ Test_work/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write ("Enter a space-separated string array: "); // Введите массив строк, разделенных пробелом 
+string[] array = Console.Readline() !.Split(' '); // Считывание введенного массива и заполнение массива с разделением на подстроки и исключением null значений 
+string[] result = new string[array.Length]; // Массив для вывода результата 
