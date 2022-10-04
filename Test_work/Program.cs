d073564c72e4cf9442830fe1134213a8eb3c850a
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-10-04 10:37:13 +0300

    Solution and debugging

--- Test_work/Program.cs
+++ Test_work/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Clear();
-Console.Write ("Enter a space-separated string array: "); // Введите массив строк, разделенных пробелом 
-string[] array = Console.Readline() !.Split(' '); // Считывание введенного массива и заполнение массива с разделением на подстроки и исключением null значений 
+﻿
+Console.WriteLine ("Enter a space-separated string array: "); // Введите массив строк, разделенных пробелом 
+string[] array = Console.ReadLine()!.Split(' '); // Считывание введенного массива и заполнение массива с разделением на подстроки и исключением null значений 
 string[] result = new string[array.Length]; // Массив для вывода результата 
+
+for (int i = 0; i < array.Length; i++) // Цикл начинается с индекса 0 и работает с шагом 1 до тех пор, пока индекс элемента меньше длины массива
+{
+    if (array[i].Length <= 3 && !string.IsNullOrWhiteSpace(array[i])) // Проверка длины строки и наличия пустых элементов в строке
+    Console.Write (result[i] = array[i] + " "); // Вывод результата
+}
